83d7f25b5de6aaeb3c802458198d63f4eb477cff
Chris Krycho <chris@krycho.com>
2015-05-13 22:58:07 -0400

    Add Python and its dependencies to configuration script.

--- configuration.sh
+++ configuration.sh
@@ -24,6 +24,10 @@ echo 'setxkbmap -model pc105 -layout us -option caps:ctrl_modifier' >> ~/.zshrc
 echo 'xinput set-button-map 7 1 2 3 5 4 6 7 8 9 10 11' >> ~/.zshrc
 
 # TODO: Install desired languages and associated tooling
+# Python dependencies
+# TODO: make this not explicitly depend on Fedora/yum
+$PKG_MGR install zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel
+
 # Setup for pyenv and pyenv-virtualenv
 git clone https://github.com/yyuu/pyenv $home/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
@@ -31,7 +35,9 @@ echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 git clone https://github.com/yyuu/pyenv-virtualenv $home/.pyenv/plugins/pyenv-virtualenv
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
+exec zsh
 
+pyenv install 3.4.3  # TODO: configure this to install desired version(s).
 
 # Last of all, update *everything*.
 $PKG_MGR update
