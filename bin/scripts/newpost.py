3b505649ef9f8d3663a08090a81c7830d09cdaad
Chris Krycho <chris@chriskrycho.com>
2016-05-13 07:50:05 -0400

    Tweak newpost script.

--- bin/scripts/newpost.py
+++ bin/scripts/newpost.py
@@ -21,7 +21,7 @@ CONTENT = SITE / 'content'
 # Existing formats.
 # TODO: create a mapping from post type to internal fields, e.g.:
 #       {'class': ['Class', 'Professor', 'School']}
-POST_TYPES = ['blog', 'class', 'devotions', 'link', 'micro', 'quotations', 
+POST_TYPES = ['blog', 'class', 'devotions', 'link', 'micro', 'quotations',
               'republish', 'school']
 DEFAULT_POST_TYPE = 'blog'
 
@@ -38,12 +38,12 @@ DATE_COMMON = "{:04}-{:02}-{:02}"
 
 def generate_post(args=None):
     args = args if args is not None else __parse_args()
-    
+
     directory = __get_directory(args.category, args.type)
     file_name = __get_file_name(args.name)
-    header = __get_header(args.name, args.category, args.tags, args.type)
+    header = __get_header(**vars(args))
     body = __get_body(args.direct_input)
-    
+
     content = header + body
     path = (directory / file_name).with_suffix('.md')
     with path.open('w') as f:
@@ -66,7 +66,7 @@ def __get_directory(category=None, type=None):
     # Otherwise, an actual category may be allowed.
     elif category is not None:
         directory /= category
-    
+
     return directory
 
 
@@ -76,25 +76,27 @@ def __get_body(use_input):
         text = sys.stdin.read()
     else:
         text = ''
-    
+
     return text
 
 
-def __get_header(name=None, category=None, tags=None, type=None):
+def __get_header(name=None, category=None, tags=None, type=None, title=None, **kwargs):
     """Build the header string to render into the file.
-    
+
     Args:
         name: The file name.
-        category: The category in which to file the post. 
+        category: The category in which to file the post.
         tags: Any tags associated with the post.
         type: The post type (format) to use with the post.
+        title: The title of the post.
+        kwargs: Any other args specified by the user (ignored)
 
-    Returns:
+e    Returns:
         A fully populated YAML header.
 
     The header will be styled so (whitespace included intentionally as it *is*
     part of the generated header):
-        
+
     ```YAML
     ---
     Title: <the title>
@@ -117,10 +119,10 @@ def __get_header(name=None, category=None, tags=None, type=None):
     time = __get_time()
     date = DATE_COMMON + " {:02}:{:02}"
     date = date.format(time.year, time.month, time.day, time.hour, time.minute)
-    
-    title = name if name is not None else date
+
+    title = title if title is not None else date
     header = TEMPLATE.format(title=title, date=date)
-    
+
     if category is not None and type != 'micro':
         header += TEMPLATE_CATEGORY.format(category=category)
 
@@ -143,12 +145,12 @@ def __get_file_name(name=None):
         name = DATE_COMMON + "-{:02}{:02}"
         name = name.format(time.year, time.month, time.day, time.hour, time.minute)
         return name
-        
+
 
 @lru_cache(maxsize=None)
 def __get_time():
     """Generate a (single!) timestamp for use with the files.
-    
+
     Returns:
         A `datetime.datetime` object.
 
@@ -173,13 +175,14 @@ def __parse_args():
                              'overridden if the post type is `micro`.')
     parser.add_argument('-t', '--tags', help='Tags associated with the post.')
     parser.add_argument('-n', '--name', help='Specify the file name to use.')
-    
+    parser.add_argument('--title', help='The post title')
+
     input_group = parser.add_mutually_exclusive_group()
     exclusion =  ' Note: -i and -e are mutually exclusive.'
     input_group.add_argument('-e', '--editor',
                              help='Specify an editor to use.' + exclusion,
                              default='subl')
-    input_group.add_argument('-i', '--direct-input', 
+    input_group.add_argument('-i', '--direct-input',
                              help='Supply input text directly.' + exclusion,
                              action='store_true',
                              default=False)
