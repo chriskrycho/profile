cf0924c796fd45fc170c9d0a99f12a2838a40183
Chris Krycho <chris@chriskrycho.com>
2016-06-01 17:30:44 -0400

    Improve `newpost` tool's post generation.

--- bin/scripts/newpost.py
+++ bin/scripts/newpost.py
@@ -27,10 +27,11 @@ DEFAULT_POST_TYPE = 'blog'
 
 # Define default text for the file.
 TEMPLATE = "---\nTitle: {title}\nDate: {date}\n"
-TEMPLATE_END = """...\n\n"""
+TEMPLATE_END = "---\n\n"
 TEMPLATE_CATEGORY = "Category: {category}\n"
 TEMPLATE_TAGS = "Tags: {tags}\n"
-TEMPLATE_TYPE = "Template: formats/{type}\n"
+TEMPLATE_SLUG = "Slug: {slug}\n"
+TEMPLATE_TYPE = "Template: formats/{post_type}\n"
 
 # Date formatting
 DATE_COMMON = "{:04}-{:02}-{:02}"
@@ -39,13 +40,15 @@ DATE_COMMON = "{:04}-{:02}-{:02}"
 def generate_post(args=None):
     args = args if args is not None else __parse_args()
 
-    directory = __get_directory(args.category, args.type)
-    file_name = __get_file_name(args.name)
-    header = __get_header(**vars(args))
+    time_for_file, time_for_meta, date_for_dir = __get_date_formats()
+
+    base_directory = __get_directory(date_for_dir, args.category, args.post_type)
+    file_name = args.name if args.name else time_for_file
+    header = __get_header(time_for_meta, time_for_file, **vars(args))
     body = __get_body(args.direct_input)
 
     content = header + body
-    path = (directory / file_name).with_suffix('.md')
+    path = (base_directory / file_name).with_suffix('.md')
     with path.open('w') as f:
         f.write(content)
 
@@ -54,14 +57,28 @@ def generate_post(args=None):
         edit_line = len(content.splitlines())
         subprocess.call([args.editor, str(path) + ':{}'.format(edit_line)])
 
+def __get_date_formats():
+    time = __get_time()
 
-def __get_directory(category=None, type=None):
+    meta_format = DATE_COMMON + " {:02}:{:02}"
+    time_for_meta = meta_format.format(time.year, time.month, time.day, time.hour, time.minute)
+
+    file_format = "{:02}-{:02}{:02}"
+    time_for_file = file_format.format(time.day, time.hour, time.minute)
+
+    dir_format = "{:02}/{:02}"
+    date_for_dir = dir_format.format(time.year, time.month)
+
+    return time_for_file, time_for_meta, date_for_dir
+
+
+def __get_directory(date_for_dir, category=None, post_type=None):
     # Start with the blog content directory.
     directory = CONTENT
 
     # Micro post type always corresponds to the microblog category.
-    if type == 'micro':
-        directory /= type
+    if post_type == 'micro':
+        directory /= post_type / Path(date_for_dir)
 
     # Otherwise, an actual category may be allowed.
     elif category is not None:
@@ -80,32 +97,40 @@ def __get_body(use_input):
     return text
 
 
-def __get_header(name=None, category=None, tags=None, type=None, title=None, **kwargs):
+def __get_header(time_for_meta: str,
+                 time_for_slug: str,
+                 name: str = None,
+                 category: str = None,
+                 tags: str = None,
+                 slug: str = None,
+                 post_type: str = None,
+                 title: str = None,
+                 **kwargs):
     """Build the header string to render into the file.
 
     Args:
         name: The file name.
         category: The category in which to file the post.
         tags: Any tags associated with the post.
-        type: The post type (format) to use with the post.
+        post_type: The post type (format) to use with the post.
         title: The title of the post.
         kwargs: Any other args specified by the user (ignored)
 
-e    Returns:
+    Returns:
         A fully populated YAML header.
 
     The header will be styled so (whitespace included intentionally as it *is*
-    part of the generated header):
+    part of the generated header), with optional elements wrapped in `[...]`:
 
     ```YAML
     ---
-    Title: <the title>
+    Title: [<the title>]
     Date: <the date>
-    Category: <the category specified as an argument>
-    Tags: <the tags specified as arguments>
-    Template: formats/<post type specified as an argument>
-    ...
-
+    [Slug: <the specified or, for microblog posts, date as slug>]
+    [Category: <the category specified as an argument>]
+    [Tags: <the tags specified as arguments>]
+    [Template: formats/<post type specified as an argument>]
+    ---
 
     ```
 
@@ -116,35 +141,29 @@ e    Returns:
         date value will be used instead.
 
     """
-    time = __get_time()
-    date = DATE_COMMON + " {:02}:{:02}"
-    date = date.format(time.year, time.month, time.day, time.hour, time.minute)
-
-    title = title if title is not None else date
-    header = TEMPLATE.format(title=title, date=date)
-
-    if category is not None and type != 'micro':
-        header += TEMPLATE_CATEGORY.format(category=category)
+    title = title if title is not None else '""'
+    header = TEMPLATE.format(title=title, date=time_for_meta)
 
     if tags is not None:
         header += TEMPLATE_TAGS.format(tags=tags)
 
-    if type != DEFAULT_POST_TYPE:
-        header += TEMPLATE_TYPE.format(type=type)
+    if post_type != DEFAULT_POST_TYPE:
+        header += TEMPLATE_TYPE.format(post_type=post_type)
+
+    # Microblog posts are a special case for category and slug.
+    if post_type == 'micro':
+        header += TEMPLATE_SLUG.format(slug=time_for_slug)
+        header += TEMPLATE_CATEGORY.format(category=post_type)
+
+    else:
+        if slug:
+            header += TEMPLATE_SLUG.format()
+
+        if category is not None:
+            header += TEMPLATE_CATEGORY.format(category=category)
 
     header += TEMPLATE_END
     return header
-
-
-def __get_file_name(name=None):
-    if name:
-        return name
-
-    else:
-        time = __get_time()
-        name = DATE_COMMON + "-{:02}{:02}"
-        name = name.format(time.year, time.month, time.day, time.hour, time.minute)
-        return name
 
 
 @lru_cache(maxsize=None)
@@ -166,8 +185,10 @@ def __parse_args():
     parser = argparse.ArgumentParser()
 
     # Positional
-    parser.add_argument('type', help='The kind of post to generate.',
-                        choices=POST_TYPES, default=DEFAULT_POST_TYPE)
+    parser.add_argument('post_type',
+                        help='The kind of post to generate.',
+                        choices=POST_TYPES,
+                        default=DEFAULT_POST_TYPE)
 
     # Optional
     parser.add_argument('-c', '--category',
